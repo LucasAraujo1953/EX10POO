f4797647ccbae6248c9f6ee93582b51d53f9b57c
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-23 18:48:23 -0300

    Add files via upload

---
+++ EX10POO/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX10POO
+{
+    internal class Pessoa
+    {
+        private double peso;
+        private double altura;
+
+        public Pessoa(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public string CalcularStatus()
+        {
+            double relacao = peso / (altura * altura);
+
+            if (relacao < 20)
+            {
+                return "Abaixo do Peso";
+            }
+            else if (relacao >= 20 && relacao < 25)
+            {
+                return "Peso Ideal";
+            }
+            else
+            {
+                return "Acima do Peso";
+            }
+        }
+    }
+}
---
+++ EX10POO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX10POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o peso da pessoa (em kg): ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura da pessoa (em metros): ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa(peso, altura);
+            string status = pessoa.CalcularStatus();
+
+            Console.WriteLine($"A pessoa está com o status: {status}");
+        }
+    }
+}
